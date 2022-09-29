c02f9c81400cf1a29830648cadf3e3c0849e6f6d
Adam Łastowski <m3lm4n@gmail.com>
2022-09-29 13:53:33 +0200

    Added medal tally section

---
+++ app/src/main/java/com/lastowski/components_test/MedalTallySection.kt
@@ -0,0 +1,19 @@
+package com.lastowski.components_test
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MedalTallySection() {
+    Text("Hello Custom Component!!")
+}
+
+@Preview
+@Composable
+fun previewMedalTallySection() {
+    MaterialTheme {
+        MedalTallySection()
+    }
+}
